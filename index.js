600e6432adf54257ffa2f02c4a10af65305453de
ooiooc <shinjh1102@naver.com>
2021-03-27 05:02:41 +0900

    add io.sockets

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const express = require('express')
 /* 설치한 socket.io 모듈 불러오기 */
 const socket = require('socket.io')
 
+/* Node.js 기본 내장 모듈 불러오기 */
+const http = require('http')
+
 /* Node.js 기본 내장 모듈 불러오기 
 fs 모듈 node.js에서 기본적으로 제공하는 모듈, 파일과 관련된 처리*/
 const fs = require('fs')
@@ -36,8 +39,8 @@ app.use('/js', express.static('./static/js'))
 
 /* Get 방식으로 / 경로에 접속하면 실행 됨 */
 app.get('/', function(request, response) {
-    fs.readFile('./index.html', funcion(err, data){
-        if(err){
+    fs.readFile('./index.html', function(err, data){
+        if(err) {
             response.send('에러')
         }else{
             response.writeHead(200, {'Content-Type':'text/html'})
@@ -46,6 +49,52 @@ app.get('/', function(request, response) {
         }
     })
   //console.log('유저가 / 으로 접속하였습니다!')
+
+io.sockets.on('connection', function(socket) { 
+  //  connection 이벤트 발생할 경우 콜백함수 실행 
+  //  io.sockets는 접속되는 모든 소켓 의미
+  //  콜백함수 안에서 해당 소켓과 통신할 코드를 작성
+
+  /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
+  socket.on('newUser', function(name){
+    console.log(name + ' 님이 접속하였습니다.')
+   
+    /* 소켓에 이름 저장해두기 */
+    socket.name = name
+   
+    /* 모든 소캣에게 전송 */
+    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'}) 
+    })
+   
+    /* 전송한 메세지 받기 */
+    socket.on('message', function(data){
+      /* 받은 데이터에 누가 보냈는지 이름을 추가 */
+      data.name = socket.name
+   
+      console.log(data)
+   
+      /* 보낸 사람을 제외한 나머지 유저에게 메세지 전송 */
+      socket.broadcast.emit('update', data)
+    })
+   
+   /* 접속 종료 */
+  socket.on('disconnect', function(){
+    console.log(socket.name + '님이 나가셨습니다.')
+   
+    /* 나가는 사람을 제외한 나머지 유저에게 메세지 전송 */
+    socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'})
+    })
+   
+    socket.on('send', function(data) {
+      console.log('전달된 메시지:', data.msg)
+    })
+   
+    socket.on('disconnect', function() {
+      //  연결되어있던 소켓과 접속이 끊어지면 자동으로 실행
+      console.log('접속 종료')
+      
+    })
+  })
 
 /* 클라이언트로 문자열 응답 */
   //response.send('Hello, Express Server!!')
